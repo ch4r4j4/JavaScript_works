740ba3ecd53d7df99bc6a2317da5b076286808f1
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-15 12:05:54 -0500

    First Project with JS
    my first project with js empezando sin rendirnos para empezar a trabjar con este lenguaje

---
+++ JSOpen/index.js
@@ -0,0 +1,19 @@
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.")
+//este el mensaje de bienvenida
+//este es el mensaje de despedida
+/*
+
+.
+.
+.
+.
+.
+
+este el comentario mas largo que estoy haciendo aqui
+.
+.
+.
+.
+.
+.
+*/
