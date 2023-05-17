e628d1761c63c825f5ad70dbb7dfd69cf88f085f
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-17 17:47:56 -0500

    createclase.js

---
+++ pruebas/clase2.js
@@ -0,0 +1,27 @@
+let num1=5;
+let num2="5"
+if(num1===num2){
+    console.log("es igual totalment");
+}else if(num1==num2){
+    console.log("es igual debil");
+}
+
+let nota=11;
+switch (nota){
+    case 0:
+        console.log("jalado");
+        break;
+    case 1:
+        console.log("jalado");
+        break;
+    case 11:
+        console.log("aprobado");
+}
+
+// comparacion negativa fuerte
+
+if(num1!==num2){
+    console.log("es diferentes");
+}else{
+    console.log("son diferenes fuertes");
+}
