f640b419d02662a494072ad966e59734f7234f5e
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-17 13:14:19 -0500

    second work of open bootcamp

---
+++ 2-listObjectArray/index.js
@@ -0,0 +1,13 @@
+var lista= [
+    "Paul",
+    23,true, 
+    new Date(1999,5,28),
+]
+var libro={
+    titulo: "las aventuras de jake",
+    autor: "Abraham Valdelomar",
+    fecha: new Date(1992,4,23),
+    url: "www.facebook.com"
+}
+
+console.log(libro.url);
