7059165b4807a5ac3a0742e50aca7761d55b8335
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-18 22:21:26 -0500

    create and advance themes about bucles

---
+++ pruebas/bucles.js
@@ -0,0 +1,38 @@
+var nums = [1,2,3];
+nums.splice(1,1);
+console.log(nums);
+nums.pop();
+console.log(nums);
+
+let lista = [1,2,3,"unique", "esica", "ana", "Lilian"];
+for(let valor of lista){/// para listas es el forof
+    console.log(valor);
+}
+
+lista.forEach(valor=>{
+    console.log(valor);
+})
+
+let persona={
+    nombre: "paul",
+    edad: 25,
+    isDeveleoper: true
+}
+let prop ="edad";
+console.log(persona[prop]);////la verdad esta parte no entinedo ni mergas
+
+
+for(let valor in persona){//// para objetos es el for in
+    console.log(valor);
+    console.log(persona[valor]); 
+}
+
+let init=0;
+let max=10
+while(init<max){console.log(init); init++;}
+
+let star =0
+do {
+    star++
+    console.log(star);
+} while (star<max);
