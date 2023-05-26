313ef1b72e18e855d5e0e79b29b7844701a0350d
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-26 17:21:59 -0500

    out the error lines

--- 3-estructurasDeControl/factorial-break.js
+++ 3-estructurasDeControl/factorial-break.js
@@ -1,7 +1,5 @@
 let factorial=1;
 let contador=1;
-let name='Paul';
-let nombre= 'el nombre de mi paruiente es, ${name} bienvenido';
 while(true){
     if(contador<=10){
         factorial=factorial*contador;
@@ -11,5 +9,3 @@ while(true){
         break;
     }
 }
-
-console.log(nombre)
