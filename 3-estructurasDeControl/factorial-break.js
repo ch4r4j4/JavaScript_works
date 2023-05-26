aca6ec27d5cfc5297c50fba1e63c81f3e22eb0aa
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-05-26 17:21:29 -0500

    change correct the exercise

--- 3-estructurasDeControl/factorial-break.js
+++ 3-estructurasDeControl/factorial-break.js
@@ -1,5 +1,7 @@
 let factorial=1;
 let contador=1;
+let name='Paul';
+let nombre= 'el nombre de mi paruiente es, ${name} bienvenido';
 while(true){
     if(contador<=10){
         factorial=factorial*contador;
@@ -9,3 +11,5 @@ while(true){
         break;
     }
 }
+
+console.log(nombre)
