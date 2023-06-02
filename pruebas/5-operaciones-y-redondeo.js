7ca1135094f29ea737102fbe95e6ec9c6beb2a13
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-06-01 23:03:36 -0500

    classe 5

---
+++ pruebas/5-operaciones-y-redondeo.js
@@ -0,0 +1,25 @@
+//sesion con numeros numbers
+let a =5;
+console.log(a);
+let b=0.1;
+console.log(b);
+let c=0.2;
+console.log(b+c);
+console.log(a+b);
+
+console.log(Math.round(c*100));
+
+console.log(Math.round((0.1+0.2)*100)/100);
+
+// video 2 de 5
+let d =2.1;
+let e =3.7;
+console.log(typeof d);
+let a_s=a.toString();
+console.log(typeof a_s);
+
+let f=3;
+let g=f*3.3;
+console.log(f.toFixed(0),typeof f);
+
+console.log(g.toPrecision(3));
