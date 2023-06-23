60269977c498e4244928a626df0ac670b5fb0986
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-06-22 22:18:41 -0500

    Update 6-arrays.js

--- pruebas/6-arrays.js
+++ pruebas/6-arrays.js
@@ -1,2 +1,17 @@
 let names = ['Analy','Paul', 'almendra',5,90];
 console.log(names);
+
+const lista1 = ["holas","jkdn",48,55,2,5,7,7];
+const lista2 = ["holas","jkdn",48,55,2,5,7,7];
+
+const lista3= lista1.concat(lista2);
+console.log(lista3);
+
+console.log(...lista3);
+
+///factor de propagacion
+const lista4 = [...lista1,...lista2]
+console.log(lista4);
+
+const lista5=[lista1,lista2]
+console.log(lista5);
